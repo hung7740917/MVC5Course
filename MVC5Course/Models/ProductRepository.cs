884b2e07d5780f492ea7774a1d2d2983d9a4f9e7
hung7740917 <hung7740917@gmail.com>
2017-03-12 16:03:51 +0800

    Day2 - 6 修改 ProductRepository 將 Stock < 500 的商品全部隱藏

--- MVC5Course/Models/ProductRepository.cs
+++ MVC5Course/Models/ProductRepository.cs
@@ -10,7 +10,25 @@ namespace MVC5Course.Models
         {
             return this.All().FirstOrDefault(p => p.ProductId == id);
         }
-	}
+
+        public override IQueryable<Product> All()
+        {
+            return base.All().Where(p => p.Stock < 500);
+        }
+
+        public IQueryable<Product> All(bool showAll)
+        {
+            if (showAll)
+            {
+                return base.All();
+            }
+            else
+            {
+                return this.All();
+            }
+        }
+
+    }
 
 	public  interface IProductRepository : IRepository<Product>
 	{
