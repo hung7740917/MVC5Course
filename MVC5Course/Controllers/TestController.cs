002586b222311d3f108edef123eba2c490334414
hung7740917 <hung7740917@gmail.com>
2017-03-18 14:30:13 +0800

    Day3 - 5 練習使用 FileResult 回傳任意檔案

--- MVC5Course/Controllers/TestController.cs
+++ MVC5Course/Controllers/TestController.cs
@@ -11,7 +11,29 @@ namespace MVC5Course.Controllers
         // GET: Test
         public ActionResult Index()
         {
+            return View("123");
+        }
+
+        public ActionResult View2()
+        {
+            return PartialView("Index");
+        }
+
+        public ActionResult View3()
+        {
             return View();
+        }
+
+        public ActionResult File()
+        {
+            //return File(@"C:\Users\Admin\Desktop\MVC5\MVC5Course\MVC5Course\Content\ball.png", "image/png");
+            return File(Server.MapPath("~/Content/ball.png"), "image/png");
+        }
+
+        public ActionResult File2()
+        {
+            //return File(@"C:\Users\Admin\Desktop\MVC5\MVC5Course\MVC5Course\Content\ball.png", "image/png");
+            return File(Server.MapPath("~/Content/ball.png"), "image/png","myBall.png");
         }
     }
 }
